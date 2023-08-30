cbe0c844cc8d446fc493825a2b8fd65d5e9a31f5
Мария Даниина <mariya199828@gmail.com~>
2023-08-30 23:29:55 +0700

    Добавляет автоматизацию для js, html и растровых  изображений

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-squoosh';
 
 // Styles
 
@@ -12,18 +17,46 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+//Html
+export const html = () => {
+  return gulp.src('sourse/*.html')
+.pipe(htmlmin({ collapseWhitespace: true }))
+.pipe(gulp.dest('build'))
+
+}
+
+//Scripts
+export const script = () => {
+  return gulp.src('sourse/js/*.js')
+  .pipe(terser)
+  .pipe(gulp.dest('build/js'))
+}
+//Images
+export const images = () => {
+  return gulp.src('sourse/img/**/*.{jpeg,png}')
+  .pipe(squoosh)
+  .pipe(gulp.dest('build/img'))
+}
+
+//WebP
+
+//Svg
+
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +74,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html,styles, server, watcher
 );
